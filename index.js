9896b79a86c0f78e65aa43233f67c0753f1569e3
gadmalaranjith <88366329+gadmalaranjith@users.noreply.github.com>
2021-10-31 09:24:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ const initializeDbAndServer = async () => {
   }
 };
 initializeDbAndServer();
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+        SELECT *
+        FROM book 
+        ORDER BY book_id;
+    `;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
